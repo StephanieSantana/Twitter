3e7fc6ec3fa5eb47459f6d0cc24df0c4fee4fb8b
stephaniesantana <stephycsantana@gmail.com>
2019-03-02 21:18:57 -0800

    Added in character count

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
+    
+    @IBOutlet weak var characterCount: UILabel!
+    
+    @IBOutlet weak var charLabel: UILabel!
     @IBAction func cancel(_ sender: Any) {
        dismiss(animated: true, completion: nil)
     }
@@ -35,19 +39,25 @@ class TweetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
+        
+        tweetTextView.delegate = self
+
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        // Set the max character limit
+        let characterLimit = 140
+        
+        // Construct what the new text would be if we allowed the user's latest edit
+        let newText = NSString(string: tweetTextView.text!).replacingCharacters(in: range, with: text)
+        characterCount.text = String(characterLimit - newText.characters.count)
+        
+        // TODO: Update Character Count Label
+        
+        // The new text should be allowed? True/False
+        return newText.characters.count < characterLimit
     }
-    */
-
 }
